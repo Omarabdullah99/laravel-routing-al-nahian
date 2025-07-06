b9cb0e34bf142fa3367b4915ad7c6460d38e151a
Omarabdullah99 <omarabdullah917303@gmail.com>
2025-07-06 19:52:43 +0600

    static parameter example

---
+++ resources/views/users.blade.php
@@ -0,0 +1,7 @@
+<h1>Top User of the month:</h1>
+<ul>
+    <li><a href="/user/1">User 1</a></li>
+    <li><a href="/user/2">User 2</a></li>
+    <li><a href="/user/3">User 3</a></li>
+    <li><a href="/user/4">User 4</a></li>
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,23 @@ Route::get('/', function () {
 
 
 Route::get('/contact',[ContactController::class,'show']);
+
+Route::get("/users", function(){
+    return view('users');
+});
+
+Route::get('/user/1', function(){
+    return 'you are now 1';
+});
+
+Route::get('/user/2', function(){
+    return 'you are now 2';
+});
+
+Route::get('/user/3', function(){
+    return 'you are now 3';
+});
+
+Route::get('/user/4', function(){
+    return 'you are now 4';
+});
