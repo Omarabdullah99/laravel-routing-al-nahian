8366a372daa919b8a892949aacd8038d02cda8c3
Omarabdullah99 <omarabdullah917303@gmail.com>
2025-07-06 20:26:54 +0600

    dynamic route parameter

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,22 @@ Route::get("/users", function(){
     return view('users');
 });
 
-Route::get('/user/1', function(){
-    return 'you are now 1';
-});
+// Route::get('/user/1', function(){
+//     return 'you are now 1';
+// });
 
-Route::get('/user/2', function(){
-    return 'you are now 2';
-});
+// Route::get('/user/2', function(){
+//     return 'you are now 2';
+// });
 
-Route::get('/user/3', function(){
-    return 'you are now 3';
-});
+// Route::get('/user/3', function(){
+//     return 'you are now 3';
+// });
 
-Route::get('/user/4', function(){
-    return 'you are now 4';
+// Route::get('/user/4', function(){
+//     return 'you are now 4';
+// });
+
+Route::get("/user/{id}", function($id){
+    return 'you are now'. $id;
 });
