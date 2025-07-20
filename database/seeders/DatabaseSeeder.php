7eec4be4641e144ffb488f0671a59f1b2bfe1ac1
Omarabdullah99 <omarabdullah917303@gmail.com>
2025-07-20 18:13:06 +0600

    seeder practice

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,14 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
-        Profile::factory(10)->create();
-        User::factory(10)->create();
+        $this->call([
+            UserSeeder::class,
+            PostSeeder::class,
+        ]);
     }
 }
